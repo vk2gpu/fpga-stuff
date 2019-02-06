a574f774fc9e981c162714526c635b686056badf
Neil Forbes-Richardson <neilo@neilo.gd>
2019-02-07 08:59:35 +1100

    use nested for loop for generate, split up get_idx into 2 functions

--- gol/verilog/gol.v
+++ gol/verilog/gol.v
@@ -8,30 +8,28 @@ module gol(
 	output reg [63:0] o_vals
 	);
 
+	function integer wrap;
+		input integer size;
+		input integer x;
+		begin
+			if(x < 0)
+				wrap = size - 1;
+			else if(x >= size)
+				wrap = 0;
+			else 
+				wrap = x;
+		end
+	endfunction
+
 	function integer get_idx;
 		input integer size;
-		input integer i_i;
 		input integer i_x;
 		input integer i_y;
 		integer x;
 		integer y;
 		begin
-			x = (i_i % size) + i_x;
-			y = (i_i / size) + i_y;
-			
-			if(x < 0) begin
-				x = size - 1;
-			end
-			if(x >= size) begin
-				x = 0;
-			end
-
-			if(y < 0) begin
-				y = size - 1;
-			end
-			if(y >= size) begin
-				y = 0;
-			end
+			x = wrap(size, i_x);
+			y = wrap(size, i_y);
 			get_idx = x + (y * size);
 		end
 	endfunction
@@ -51,24 +49,26 @@ module gol(
 
 	genvar i, j;
 	generate
-		for (i=0; i<64; i=i+1) begin : gen_gol_cell_x
-			gol_cell gol_cell_xy (
-				.i_clk(i_clk),
-				.i_reset(i_reset || s_reset_delay != 0),
-				.i_val(i_vals[i]),
-				.i_neighbours(
-					{
-						s_next_vals[get_idx(8, i, -1, -1)],
-						s_next_vals[get_idx(8, i, 0, -1)],
-						s_next_vals[get_idx(8, i, 1, -1)],
-						s_next_vals[get_idx(8, i, -1, 0)],
-						s_next_vals[get_idx(8, i, 1, 0)],
-						s_next_vals[get_idx(8, i, -1, 1)],
-						s_next_vals[get_idx(8, i, 0, 1)],
-						s_next_vals[get_idx(8, i, 1, 1)]
-					}),
-				.o_val(s_next_vals[i])
-			);
+		for (j=0; j<8; j=j+1) begin : gen_gol_cell_y
+			for (i=0; i<8; i=i+1) begin : gen_gol_cell_x
+				gol_cell gol_cell_xy (
+					.i_clk(i_clk),
+					.i_reset(i_reset || s_reset_delay != 0),
+					.i_val(i_vals[get_idx(8, i, j)]),
+					.i_neighbours(
+						{
+							s_next_vals[get_idx(8, i + -1, j + -1)],
+							s_next_vals[get_idx(8, i +  0, j + -1)],
+							s_next_vals[get_idx(8, i +  1, j + -1)],
+							s_next_vals[get_idx(8, i + -1, j +  0)],
+							s_next_vals[get_idx(8, i +  1, j +  0)],
+							s_next_vals[get_idx(8, i + -1, j +  1)],
+							s_next_vals[get_idx(8, i +  0, j +  1)],
+							s_next_vals[get_idx(8, i +  1, j +  1)]
+						}),
+					.o_val(s_next_vals[get_idx(8, i, j)])
+				);
+			end
 		end
 	endgenerate
 endmodule
